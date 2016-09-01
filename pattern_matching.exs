0d8d91904977032f6dc557f2053e7a4957a23253
Jason Pilz <jasonpilz@gmail.com>
2016-08-31 20:54:54 -0700

    Revise pattern_matching examples to conform to the style guide

--- pattern_matching.exs
+++ pattern_matching.exs
@@ -1,26 +1,22 @@
-"""
-  -> Elixir calls '=' the 'match operator'.
 
-  -> Variables bind once (per match)
+#  -> Elixir calls '=' the 'match operator'.
+#  -> Variables bind once (per match)
+#  -> An underscore '_' can be used as a wildcard in matching
+#  -> A Caret '^' is called a 'pin operator' and will use a variables existing
+#     value from previous match.
 
-  -> An underscore '_' can be used as a wildcard in matching
-
-  -> A Caret '^' is called a 'pin operator' and will use a variables existing
-     value from previous match.
-"""
 
 ## Which of the following will match?
-[ a, b, a ] = [ 1, 2, 3 ]
-[ a, b, a ] = [ 1, 1, 2 ]
-[ a, b, a ] = [ 1, 2, 1 ] # This will match
+[a, b, a] = [1, 2, 3]
+[a, b, a] = [1, 1, 2]
+[a, b, a] = [1, 2, 1] # This will match
 
 ## Which will match?
 a = 2
 
-[ a, b, a ] = [ 1, 2, 3 ]
-[ a, b, a ] = [ 1, 1, 2 ]
+[a, b, a] = [1, 2, 3]
+[a, b, a] = [1, 1, 2]
 a = 1                     # This will match
 ^a = 2
 ^a = 1                    # This will match
 ^a = 2 - a                # This will match
-
